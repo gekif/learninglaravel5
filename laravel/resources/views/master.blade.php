382ffd724fadca72934581860a43eb412370f589
Gekif <dzulfikar.maulana@gmail.com>
2020-02-18 21:15:58 +0700

    Chapter 2 Building Our First Website: Refine our website layouts

--- laravel/resources/views/master.blade.php
+++ laravel/resources/views/master.blade.php
@@ -14,7 +14,7 @@
 
 @yield('content')
 
-<script src="//code.jquery.com/jquery-1.11.3.min.js"> </script>
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"> </script>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
